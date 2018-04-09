a2ccb93b2c282d36015b18f99dc2c85753463a3c
HoangDoanMinh <tonyha1x@gmail.com>
2018-04-09 10:29:33 +0700

    Vesion 02

---
+++ ThucHanh02/src/thuchanh02/ThucHanh02.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thuchanh02;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DOANMINH
+ */
+public class ThucHanh02 extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public ThucHanh02() {
+        setTitle("Hoàng Đoàn Minh");
+        setSize(300,200);
+        setLayout(new FlowLayout());
+        setVisible(true);
+    
+}
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ThucHanh02 ThucHanh02 = new ThucHanh02();
+    }
+    
+}
