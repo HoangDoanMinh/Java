b2844c40022de33fa44fcabf8622145836840138
HoangDoanMinh <tonyha1x@gmail.com>
2018-04-12 09:28:51 +0700

    Vesion03

---
+++ ThucHanh02/src/thuchanh02/SinhVien.java
@@ -0,0 +1,56 @@
+
+package thuchanh02;
+
+public class SinhVien 
+{
+    private String HoTen;
+    private int NamSinh;
+    private String GioiTinh;
+
+    /**
+     * @return the HoTen
+     */
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    /**
+     * @param HoTen the HoTen to set
+     */
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    /**
+     * @return the NamSinh
+     */
+    public int getNamSinh() {
+        return NamSinh;
+    }
+
+    /**
+     * @param NamSinh the NamSinh to set
+     */
+    public void setNamSinh(int NamSinh) {
+        this.NamSinh = NamSinh;
+    }
+
+    /**
+     * @return the GioiTinh
+     */
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    /**
+     * @param GioiTinh the GioiTinh to set
+     */
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    void getGioiTinh(String GioiTinh) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
